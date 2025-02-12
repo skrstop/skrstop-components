a8574615fcce63e9cfbee7344bd2258fd6ea88e4
shihua.jiang <4086824@qq.com>
2025-02-12 10:31:49 +0800

    fix: starter-web兼容springdoc的接入

--- components-core/components-core-common/src/main/java/com/skrstop/framework/components/core/common/response/page/ListSimplePageData.java
+++ components-core/components-core-common/src/main/java/com/skrstop/framework/components/core/common/response/page/ListSimplePageData.java
@@ -2,6 +2,8 @@ package com.skrstop.framework.components.core.common.response.page;
 
 import java.io.Serial;
 import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 /**
  * @author 蒋时华
@@ -11,4 +13,12 @@ import java.util.List;
 public class ListSimplePageData<T> extends SimplePageData<List<T>> {
     @Serial
     private static final long serialVersionUID = -2419450763177948411L;
+
+    public <R> ListSimplePageData<R> convert(Function<? super T, ? extends R> mapper) {
+        ListSimplePageData<R> convertPage = new ListSimplePageData<>();
+        List<R> collect = this.getRows().stream().map(mapper).collect(Collectors.toList());
+        convertPage.setRows(collect);
+        return convertPage;
+    }
+
 }
