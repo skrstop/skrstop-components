6db97dd882fa721c2a48f987f5a4fb6b68799d88
shihua.jiang <4086824@qq.com>
2025-05-29 17:32:17 +0800

    fix: ResponseOutUtil bug修复

--- components-starters/components-starter-web/src/main/java/com/skrstop/framework/components/starter/web/utils/ResponseOutUtil.java
+++ components-starters/components-starter-web/src/main/java/com/skrstop/framework/components/starter/web/utils/ResponseOutUtil.java
@@ -1,5 +1,6 @@
 package com.skrstop.framework.components.starter.web.utils;
 
+import com.skrstop.framework.components.core.common.response.Result;
 import com.skrstop.framework.components.core.common.response.core.IResult;
 import com.skrstop.framework.components.util.enums.CharSetEnum;
 import com.skrstop.framework.components.util.enums.ContentTypeEnum;
@@ -7,6 +8,7 @@ import com.skrstop.framework.components.util.serialization.json.FastJsonUtil;
 import com.skrstop.framework.components.util.value.data.CollectionUtil;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import reactor.core.publisher.Flux;
@@ -23,45 +25,41 @@ import java.nio.charset.Charset;
  * @date 2018/11/8
  */
 public class ResponseOutUtil extends ServletUtil {
-    public static String DEFAULT_CHARSET = CharSetEnum.UTF8.toString();
-    public static String DEFAULT_APPLICATION = ContentTypeEnum.APPLICATION_JSON_UTF8.toString();
 
-    public static Mono<Void> fluxOut(ServerHttpResponse response, IResult IResult, Class errorCodeCls) throws IOException {
-        response.getHeaders().setContentType(MediaType.APPLICATION_JSON);
-        response.getHeaders()
-                .setAcceptCharset(CollectionUtil.newArrayList(Charset.forName(CharSetEnum.UTF8.toString())));
-        String body = FastJsonUtil.toJson(errorCodeCls);
-        DataBuffer wrap = response.bufferFactory().wrap(body.getBytes(CharSetEnum.UTF8.toString()));
-        return response.writeWith(Flux.just(wrap));
+    public static Mono<Void> fluxOut(ServerHttpResponse response, IResult IResult, HttpStatus httpStatus) throws IOException {
+        response.setStatusCode(httpStatus);
+        return fluxOut(response, IResult);
     }
 
-    public static Mono<Void> fluxOut(ServerHttpResponse response, IResult IResult, Class errorCodeCls, CharSetEnum charSetEnum,
-                                     ContentTypeEnum contentTypeEnum) throws IOException {
+    public static Mono<Void> fluxOut(ServerHttpResponse response, IResult IResult) throws IOException {
+        return fluxOut(response, IResult, CharSetEnum.UTF8, ContentTypeEnum.APPLICATION_JSON_UTF8);
+    }
+
+    public static Mono<Void> fluxOut(ServerHttpResponse response, IResult IResult, CharSetEnum charSetEnum, ContentTypeEnum contentTypeEnum) throws IOException {
         response.getHeaders().setContentType(MediaType.parseMediaType(contentTypeEnum.getContentType()));
         response.getHeaders()
                 .setAcceptCharset(CollectionUtil.newArrayList(Charset.forName(charSetEnum.getCharSet())));
-        String body = FastJsonUtil.toJson(errorCodeCls);
+        String body = FastJsonUtil.toJson(new Result<>(IResult));
         DataBuffer wrap = response.bufferFactory().wrap(body.getBytes(charSetEnum.getCharSet()));
         return response.writeWith(Flux.just(wrap));
     }
 
-    public static void out(HttpServletResponse response, IResult IResult, Class errorCodeCls) throws IOException {
-        response.setCharacterEncoding(DEFAULT_CHARSET);
-        response.setContentType(DEFAULT_APPLICATION);
-
-        try (PrintWriter writer = response.getWriter()) {
-            writer.print(FastJsonUtil.toJson(errorCodeCls));
-        }
+    public static void out(HttpServletResponse response, IResult IResult, HttpStatus httpStatus) throws IOException {
+        out(response, IResult, CharSetEnum.UTF8, ContentTypeEnum.APPLICATION_JSON_UTF8, httpStatus);
     }
 
-    public static void out(HttpServletResponse response, IResult IResult, Class errorCodeCls, CharSetEnum charSetEnum,
-                           ContentTypeEnum contentTypeEnum)
+    public static void out(HttpServletResponse response, IResult IResult) throws IOException {
+        out(response, IResult, CharSetEnum.UTF8, ContentTypeEnum.APPLICATION_JSON_UTF8, HttpStatus.OK);
+    }
+
+    public static void out(HttpServletResponse response, IResult IResult, CharSetEnum charSetEnum, ContentTypeEnum contentTypeEnum, HttpStatus httpStatus)
             throws IOException {
         response.setCharacterEncoding(charSetEnum.toString());
         response.setContentType(contentTypeEnum.toString());
+        response.setStatus(httpStatus.value());
 
         try (PrintWriter writer = response.getWriter()) {
-            writer.print(FastJsonUtil.toJson(errorCodeCls));
+            writer.print(FastJsonUtil.toJson(new Result<>(IResult)));
         }
     }
 }
