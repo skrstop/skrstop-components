4719252107b4f3f93071ed3d11de969147447d7b
shihua.jiang <4086824@qq.com>
2025-09-23 18:48:11 +0800

    refactor: 重构database、mongodb组件重构

--- components-utils/components-util-constant/src/main/java/com/skrstop/framework/components/util/constant/DateFormatConst.java
+++ components-utils/components-util-constant/src/main/java/com/skrstop/framework/components/util/constant/DateFormatConst.java
@@ -6,87 +6,101 @@ package com.skrstop.framework.components.util.constant;
  */
 public interface DateFormatConst {
 
-//-------------------------------------------------------------------------------------------------------------------------------- Normal
+    /**
+     * 年月日期格式：yyyy-MM
+     */
+    String NORM_YEAR_MONTH_PATTERN = "yyyy-MM";
+
     /**
      * 标准日期格式：yyyy-MM-dd
      */
-    public final static String NORM_DATE_PATTERN = "yyyy-MM-dd";
+    String NORM_DATE_PATTERN = "yyyy-MM-dd";
 
     /**
      * 标准时间格式：HH:mm:ss
      */
-    public final static String NORM_TIME_PATTERN = "HH:mm:ss";
+    String NORM_TIME_PATTERN = "HH:mm:ss";
 
     /**
      * 标准日期时间格式，精确到分：yyyy-MM-dd HH:mm
      */
-    public final static String NORM_DATETIME_MINUTE_PATTERN = "yyyy-MM-dd HH:mm";
+    String NORM_DATETIME_MINUTE_PATTERN = "yyyy-MM-dd HH:mm";
 
     /**
      * 标准日期时间格式，精确到秒：yyyy-MM-dd HH:mm:ss
      */
-    public final static String NORM_DATETIME_PATTERN = "yyyy-MM-dd HH:mm:ss";
+    String NORM_DATETIME_PATTERN = "yyyy-MM-dd HH:mm:ss";
 
     /**
      * 标准日期时间格式，精确到毫秒：yyyy-MM-dd HH:mm:ss.SSS
      */
-    public final static String NORM_DATETIME_MS_PATTERN = "yyyy-MM-dd HH:mm:ss.SSS";
+    String NORM_DATETIME_MS_PATTERN = "yyyy-MM-dd HH:mm:ss.SSS";
+
+    /**
+     * 标准日期时间格式，精确到微秒：yyyy-MM-dd HH:mm:ss.SSSSSS
+     */
+    String NORM_DATETIME_US_PATTERN = "yyyy-MM-dd HH:mm:ss.SSSSSS";
+
+    /**
+     * 标准日期时间格式，精确到纳秒：yyyy-MM-dd HH:mm:ss.SSS
+     */
+    String NORM_DATETIME_NS_PATTERN = "yyyy-MM-dd HH:mm:ss.SSSSSSSSS";
 
     /**
      * 标准日期格式：yyyy年MM月dd日
      */
-    public final static String CHINESE_DATE_PATTERN = "yyyy年MM月dd日";
+    String CHINESE_DATE_PATTERN = "yyyy年MM月dd日";
 
     //-------------------------------------------------------------------------------------------------------------------------------- Pure
     /**
      * 标准日期格式：yyyyMMdd
      */
-    public final static String PURE_DATE_PATTERN = "yyyyMMdd";
+    String PURE_DATE_PATTERN = "yyyyMMdd";
 
     /**
      * 标准日期格式：HHmmss
      */
-    public final static String PURE_TIME_PATTERN = "HHmmss";
+    String PURE_TIME_PATTERN = "HHmmss";
 
     /**
      * 标准日期格式：yyyyMMddHHmmss
      */
-    public final static String PURE_DATETIME_PATTERN = "yyyyMMddHHmmss";
+    String PURE_DATETIME_PATTERN = "yyyyMMddHHmmss";
 
     /**
      * 标准日期格式：yyyyMMddHHmmssSSS
      */
-    public final static String PURE_DATETIME_MS_PATTERN = "yyyyMMddHHmmssSSS";
+    String PURE_DATETIME_MS_PATTERN = "yyyyMMddHHmmssSSS";
 
     //-------------------------------------------------------------------------------------------------------------------------------- Others
     /**
      * HTTP头中日期时间格式：EEE, dd MMM yyyy HH:mm:ss z
      */
-    public final static String HTTP_DATETIME_PATTERN = "EEE, dd MMM yyyy HH:mm:ss z";
+    String HTTP_DATETIME_PATTERN = "EEE, dd MMM yyyy HH:mm:ss z";
 
     /**
      * JDK中日期时间格式：EEE MMM dd HH:mm:ss zzz yyyy
      */
-    public final static String JDK_DATETIME_PATTERN = "EEE MMM dd HH:mm:ss zzz yyyy";
+    String JDK_DATETIME_PATTERN = "EEE MMM dd HH:mm:ss zzz yyyy";
 
     /**
      * UTC时间：yyyy-MM-dd'T'HH:mm:ss'Z'
      */
-    public final static String UTC_PATTERN = "yyyy-MM-dd'T'HH:mm:ss'Z'";
+    String UTC_PATTERN = "yyyy-MM-dd'T'HH:mm:ss'Z'";
 
     /**
      * UTC时间：yyyy-MM-dd'T'HH:mm:ssZ
      */
-    public final static String UTC_WITH_ZONE_OFFSET_PATTERN = "yyyy-MM-dd'T'HH:mm:ssZ";
+    String UTC_WITH_ZONE_OFFSET_PATTERN = "yyyy-MM-dd'T'HH:mm:ssZ";
 
     /**
      * UTC时间：yyyy-MM-dd'T'HH:mm:ss.SSS'Z'
      */
-    public final static String UTC_MS_PATTERN = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'";
+    String UTC_MS_PATTERN = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'";
 
     /**
      * UTC时间：yyyy-MM-dd'T'HH:mm:ssZ
      */
-    public final static String UTC_MS_WITH_ZONE_OFFSET_PATTERN = "yyyy-MM-dd'T'HH:mm:ss.SSSZ";
+    String UTC_MS_WITH_ZONE_OFFSET_PATTERN = "yyyy-MM-dd'T'HH:mm:ss.SSSZ";
 
 }
