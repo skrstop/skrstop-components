f7664808e4280f5717e277667df5893060de423b
shihua.jiang <4086824@qq.com>
2026-05-22 11:15:25 +0800

    feat: 删除select 1的自定义环境变量

--- components-starters/components-starter-database/src/main/java/com/skrstop/framework/components/starter/database/constant/GlobalConfigConst.java
+++ components-starters/components-starter-database/src/main/java/com/skrstop/framework/components/starter/database/constant/GlobalConfigConst.java
@@ -10,7 +10,7 @@ public interface GlobalConfigConst {
 
     String DATABASE_PREFIX = "skrstop.database.config";
     String DATABASE_DYNAMIC = "skrstop.database.dynamic";
-    String DATABASE_CONFIG_CHECK_DRUID_USE_SELECT1 = DATABASE_PREFIX + ".checkDruidUseSelect1";
+    String DATABASE_CONFIG_CHECK_DRUID_USE_PING_METHOD = DATABASE_PREFIX + ".usePingMethod";
     String DATABASE_CONFIG_CLASS_CLASS_LOCATION = DATABASE_PREFIX + ".mapper-class-location";
     String DEFAULT_MAPPER_XML_LOCATION = "classpath*:mapper/*.xml";
 
